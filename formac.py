f3f5e569fe7d16d234d44130e9b977f6d930b340
Christopher Rigby <christopher.rigby@onthebeach.co.uk>
2017-04-24 10:38:47 +0100

    Add option for lowercase and shortcut to cisco standard

--- formac.py
+++ formac.py
@@ -8,12 +8,19 @@ parser = argparse.ArgumentParser(description='Format MAC addresses')
 parser.add_argument('mac_address_raw', metavar='nn:nn:nn:nn:nn:nn', type=str, nargs=1,
         help='a MAC address in any format')
 parser.add_argument('-U', '--upcase', help='Make output uppercase', action='store_true')
+parser.add_argument('-l', '--lowercase', help='Make output lowercase', action='store_true')
 parser.add_argument('-g', '--group', type=int, choices=[2,4], help='Byte grouping size')
 parser.add_argument('-s', '--seperator', type=str, choices=[':','.','-','none'], help='seperator character')
+parser.add_argument('--cisco', dest='cisco', action='store_true', help='Shortcut to --group 4 --seperator .')
+parser.set_defaults(cisco=False)
 
 args = parser.parse_args()
 mac_address_raw = args.mac_address_raw[0]
 mac_address = mac_address_raw.translate(None, '.:-_')
+
+if args.cisco:
+    args.group = 4
+    args.seperator='.'
 
 def is_hex(s):
     return all(c in string.hexdigits for c in s)
@@ -26,6 +33,12 @@ def is_mac_address(s):
 
 def is_upcase():
     if args.upcase:
+        return True
+    else:
+        return False
+
+def is_lowercase():
+    if args.lowercase:
         return True
     else:
         return False
@@ -48,6 +61,8 @@ def seperator():
 def generate_mac(mac_address, seperator, interval):
     if is_upcase():
         mac_address = mac_address.upper()
+    elif is_lowercase():
+        mac_address = mac_address.lower()
 
     return seperator.join(mac_address[i:i+interval]
             for i in range(0, len(mac_address), interval))
