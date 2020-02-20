becf90f923bb93d0f0b061cc906c9817350b8f0e
Christopher Rigby <christopher.rigby@onthebeach.co.uk>
2020-02-20 14:00:11 +0000

    Run through black code formatter

--- formac.py
+++ formac.py
@@ -4,27 +4,51 @@ import sys
 import string
 import argparse
 
-parser = argparse.ArgumentParser(description='Format MAC addresses')
-parser.add_argument('mac_address_raw', metavar='nn:nn:nn:nn:nn:nn', type=str, nargs=1,
-        help='a MAC address in any format')
-parser.add_argument('-U', '--upcase', help='Make output uppercase', action='store_true')
-parser.add_argument('-l', '--lowercase', help='Make output lowercase', action='store_true')
-parser.add_argument('-g', '--group', type=int, choices=[2,4], help='Byte grouping size')
-parser.add_argument('-s', '--seperator', type=str, choices=[':','.','-','none'], help='seperator character')
-parser.add_argument('--cisco', dest='cisco', action='store_true', help='Shortcut to --group 4 --seperator .')
+parser = argparse.ArgumentParser(description="Format MAC addresses")
+parser.add_argument(
+    "mac_address_raw",
+    metavar="nn:nn:nn:nn:nn:nn",
+    type=str,
+    nargs=1,
+    help="a MAC address in any format",
+)
+parser.add_argument("-U", "--upcase", help="Make output uppercase", action="store_true")
+parser.add_argument(
+    "-l", "--lowercase", help="Make output lowercase", action="store_true"
+)
+parser.add_argument(
+    "-g", "--group", type=int, choices=[2, 4], help="Byte grouping size"
+)
+parser.add_argument(
+    "-s",
+    "--seperator",
+    type=str,
+    choices=[":", ".", "-", "none"],
+    help="seperator character",
+)
+parser.add_argument(
+    "--cisco",
+    dest="cisco",
+    action="store_true",
+    help="Shortcut to --group 4 --seperator .",
+)
 parser.set_defaults(cisco=False)
 
 args = parser.parse_args()
 mac_address_raw = args.mac_address_raw[0]
-mac_address = mac_address_raw.translate(mac_address_raw.maketrans('.:-_','.:-_','.:-_'))
+mac_address = mac_address_raw.translate(
+    mac_address_raw.maketrans(".:-_", ".:-_", ".:-_")
+)
 
 if args.cisco:
     args.group = 4
-    args.seperator='.'
-    args.lowercase=True
+    args.seperator = "."
+    args.lowercase = True
+
 
 def is_hex(s):
     return all(c in string.hexdigits for c in s)
+
 
 def is_mac_address(s):
     if is_hex(s) and (len(s) == 12):
@@ -32,11 +56,13 @@ def is_mac_address(s):
     else:
         return False
 
+
 def is_upcase():
     if args.upcase:
         return True
     else:
         return False
+
 
 def is_lowercase():
     if args.lowercase:
@@ -44,20 +70,23 @@ def is_lowercase():
     else:
         return False
 
+
 def interval():
     if args.group:
         return args.group
     else:
         return 2
 
+
 def seperator():
     if args.seperator:
-        if args.seperator == 'none':
-            return ''
+        if args.seperator == "none":
+            return ""
         else:
             return args.seperator
     else:
-        return ':'
+        return ":"
+
 
 def generate_mac(mac_address, seperator, interval):
     if is_upcase():
@@ -65,8 +94,10 @@ def generate_mac(mac_address, seperator, interval):
     elif is_lowercase():
         mac_address = mac_address.lower()
 
-    return seperator.join(mac_address[i:i+interval]
-            for i in range(0, len(mac_address), interval))
+    return seperator.join(
+        mac_address[i : i + interval] for i in range(0, len(mac_address), interval)
+    )
+
 
 if is_mac_address(mac_address):
     print(generate_mac(mac_address, seperator(), interval()))
