431101b5931f652f33c67534b20f577c15e2c599
Chris Rigby <chris@crigby.net>
2023-04-28 12:46:29 +0100

    Simple OUI lookup from Wireshark's repo

--- formac.py
+++ formac.py
@@ -3,6 +3,8 @@
 import sys
 import string
 import argparse
+import requests
+import re
 
 parser = argparse.ArgumentParser(description='Format MAC addresses')
 parser.add_argument('mac_address_raw', metavar='nn:nn:nn:nn:nn:nn', type=str, nargs=1,
@@ -12,6 +14,7 @@ parser.add_argument('-l', '--lowercase', help='Make output lowercase', action='s
 parser.add_argument('-g', '--group', type=int, choices=[2,4], help='Byte grouping size')
 parser.add_argument('-s', '--seperator', type=str, choices=[':','.','-','none'], help='seperator character')
 parser.add_argument('--cisco', dest='cisco', action='store_true', help='Shortcut to --group 4 --seperator .')
+parser.add_argument('-o', '--ouilookup', help="Do an OUI lookup against Wireshark's public OUI database", action='store_true')
 parser.set_defaults(cisco=False)
 
 args = parser.parse_args()
@@ -68,8 +71,31 @@ def generate_mac(mac_address, seperator, interval):
     return seperator.join(mac_address[i:i+interval]
             for i in range(0, len(mac_address), interval))
 
+def oui_lookup(mac_address):
+    mac_addr = generate_mac(mac_address, ':', 2)
+    oui = ':'.join(mac_addr.split(':')[0:3]).upper().strip()
+    r = requests.get('https://gitlab.com/wireshark/wireshark/-/raw/master/manuf')
+    if r.status_code == 200:
+        matches = re.findall("%s.*" % oui, r.text, re.MULTILINE)
+        if matches:
+            ret = matches
+        else:
+            ret = False
+    else:
+        ret = False
+
+    return ret
+
 if is_mac_address(mac_address):
     print(generate_mac(mac_address, seperator(), interval()))
+    if args.ouilookup:
+        matches = oui_lookup(mac_address)
+
+        if matches:
+            for match in matches:
+                print(match)
+        else:
+            print('No OUI matches found')
 else:
     print("%s is not a valid MAC address" % mac_address_raw)
     exit(1)
